49ac4e791dd65aa2c647ed6b745f8cc331054e22
shahanxd <shahanxd@gmail.com>
2025-06-06 16:02:30 +0530

    added operation logics for calculations

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-// JavaScript functionality will be implemented here
-
 console.log('Calculator initialized');
+const display = document.getElementById('display');
+
+function appendToDisplay(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function calculateResult() {
+    const ans = eval(display.value);
+    display.value = ans;
+}
